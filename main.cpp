f1d5d8b95314aa9cbea0920770d81f8d70ebc160
33kB SRAM <wikylim@student.usm.my>
2023-12-15 19:20:58 +0800

    added files

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/*  EEE123 COMPUTER PROGRAMMING MINI PROJECT | SEMESTER 1 | SESSION 23/24
+    
+    GROUP:      23
+    TITLE:      Tabular Image Data to Matrix Data Converter (for OpenCV purposes)
+    REVISION:   0
+
+    MEMBERS DETAIL
+    ROLES
+
+
+
+
+
+*/
+
+#if __linux__                                           //DO NOT EDIT OR REMOVE
+    #define CATCH_CONFIG_RUNNER                         //DO NOT EDIT OR REMOVE
+    #include "catch.hpp"                                //DO NOT EDIT OR REMOVE
+    int runCatchTests(int argc, char* const argv[]){    //DO NOT EDIT OR REMOVE
+        return Catch::Session().run(argc, argv);}       //DO NOT EDIT OR REMOVE
+#endif                                                  //DO NOT EDIT OR REMOVE
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>     
+
+#include "functions.hpp"
+
+int main(int argc, char* const argv[]){                 //DO NOT EDIT OR REMOVE
+    #if __linux__                                       //DO NOT EDIT OR REMOVE
+        return runCatchTests(argc, argv);               //DO NOT EDIT OR REMOVE
+    #endif                                              //DO NOT EDIT OR REMOVE
+
+    // s t a r t   h e r e . . .
+
+
+
+
+
+
+
+
+
+
+
+}
