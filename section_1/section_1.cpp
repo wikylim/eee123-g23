dcc111ac1a13217ff4fc6989ba7ecde96854eb0b
Aydin21 <aydin.ghaz@student.usm.my>
2023-12-29 20:22:12 +0800

    aydin

--- section_1/section_1.cpp
+++ section_1/section_1.cpp
@@ -1,5 +1,54 @@
 //welcome...
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 int main(){
+char line;
+string temp = "okay";
+//tuple<int , int> data;
+int number, i=0, j=0, k=0, repeat =0, raw1[1000][2], change;
 
+string filePath = "C:\\cpp\\aaaaa\\set 2\\raw1.txt"; 
+ifstream input_file(filePath);
+if (!input_file.is_open()) {
+    cerr << "Could not open the file - '" << filePath << "'" << endl;
+    return EXIT_FAILURE;
+}
+//input data
+input_file >> temp;
+input_file >> line;
+
+while (input_file >> line){  
+    if (input_file >> number){    
+        raw1 [i][j] = number;
+
+        if (j == 0){
+            j+=1; 
+        }
+        else {
+            j-=1;
+            i++;
+            input_file >> temp;
+            repeat ++;
+        }
+    }
+}
+input_file.close();
+
+//print collected data
+cout << "[";
+for (i=0 ; i < repeat ; i++){
+    j=0;
+    cout << "(" <<raw1 [i][j] << " , " ;
+    while (j == 0){
+        j = 1;
+        cout << raw1 [i][j] << ") " ; 
+        
+    }
+}
+cout << "]";
+
+return 0;
 }
--- section_2/section_2.cpp
+++ section_2/section_2.cpp
@@ -1,5 +1,33 @@
 //welcome...
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
-int main(){
+int main() {
+    char line;
+    string temp = "okay";
+    int number, i=0, j=0, k=0, repeat =0, raw1[1000][2], change;
+    //sort for y coordinate
+    cout << "\n\n Sorted y coordinate\n";
+    for (i = 0; i < repeat; i++) {
+        for (k = i + 1; k < repeat; k++) {
+            if (raw1[i][1] >= raw1[k][1]) {
+                change = raw1[i][1];
+                raw1[i][1] = raw1[k][1];
+                raw1[k][1] = change;
+            }
+        }
+    }
+    for (i=0 ; i < repeat ; i++){
+        j=0;
+        cout << "(" <<raw1 [i][j] << " , " ;
+        while (j == 0){
+            j = 1;
+            cout << raw1 [i][j] << ") " ; 
+    }
+}
 
+
+return 0;
 }
